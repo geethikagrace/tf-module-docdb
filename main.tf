8bf8ef51605f7cb4183e6c1aa44c60684fed0a03
geethika <geethilka2219@gmail.com>
2023-06-23 00:24:52 -0700

    docdb

--- main.tf
+++ main.tf
@@ -42,8 +42,8 @@ resource "aws_docdb_cluster" "main" {
   cluster_identifier               = "${var.name}-${var.env}"
   engine                           = "docdb"
   engine_version                   = var.engine_version
-  master_username                  = data.aws_ssm_parameter.db_user.value
-  master_password                  = data.aws_ssm_parameter.db_pass.value
+  master_username                  =  "roboshop"
+  master_password                  = "roboshop123456"
   backup_retention_period          = 5
   preferred_backup_window          = "07:00-09:00"
   skip_final_snapshot              = true
